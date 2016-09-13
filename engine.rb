c5907646baa61f60ab290bc752fcdb7e94d25f78
Max Jacobson <max@hardscrabble.net>
2016-09-12 22:11:05 -0400

    new PR

--- engine.rb
+++ engine.rb
@@ -29,7 +29,7 @@ class Engine
         if hello
           if hello
             if hello
-              "bye?"
+              "bye?                                                                 so long."
             end
           end
         end
